63e9d18d9d3ffdf9020d61dc62299f66ca40c2b7
manav sinha <manav.s@gopherslab.com>
2022-05-03 14:16:14 +0530

    random ques order func added

--- quiz-game/QuizGame/game.go
+++ quiz-game/QuizGame/game.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"time"
 )
 
@@ -38,17 +39,31 @@ func QuizGame() {
 	tc := make(chan bool)
 	ans := make(chan string)
 	score, notAtTimeScore := 0, 0
+	rand.Seed(time.Now().Unix())
+	mp := make(map[int]bool, len(quiz.ques))
+	id := make([]int, len(quiz.ques))
+	for i := 0; i < len(quiz.ques); i++ {
+		for {
+			tmp := rand.Intn(len(quiz.ques))
+			if _, ok := mp[tmp]; ok == false {
+				mp[tmp] = true
+				id[i] = tmp
+				break
+			}
+		}
+	}
+	// fmt.Println(id)
 	fmt.Println("Quiz is Ready! Are you ready to go:\nPress enter to start")
 	fmt.Scanf("%s")
-	for _, ques := range quiz.ques {
-		fmt.Print(ques.Question, ":")
+	for _, i := range id {
+		fmt.Print(quiz.ques[i].Question, ":")
 		go timer(tc, *t)
-		go handler(ans, ques.Solution)
+		go handler(ans, quiz.ques[i].Solution)
 		select {
 		case <-tc:
 			fmt.Print("\n\nTime's Up\nit wont count, but have a go:")
 			sol := <-ans
-			if sol == ques.Solution {
+			if sol == quiz.ques[i].Solution {
 				notAtTimeScore++
 			}
 			fmt.Println()
@@ -56,7 +71,7 @@ func QuizGame() {
 		case sol := <-ans:
 			fmt.Print("\nYou're Fast\nGearing up next Question\n\n")
 			<-tc
-			if sol == ques.Solution {
+			if sol == quiz.ques[i].Solution {
 				score++
 			}
 			break
