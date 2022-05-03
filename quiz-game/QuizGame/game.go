20b6c4e7454b6eb0fc4f8a32e93fb74268c84cdc
manav sinha <manav.s@gopherslab.com>
2022-05-03 12:47:37 +0530

    individual ques timer added

--- quiz-game/QuizGame/game.go
+++ quiz-game/QuizGame/game.go
@@ -1,11 +1,9 @@
 package QuizGame
 
 import (
-	"encoding/csv"
 	"flag"
 	"fmt"
-	"os"
-	"strings"
+	"log"
 	"time"
 )
 
@@ -16,51 +14,54 @@ func init() {
 	total = 0
 }
 
-func Timer(ch chan bool, timer int) {
-	select {
-	case <-time.After(time.Duration(timer) * time.Second):
-		ch <- true
-	}
-
+func timer(ch chan bool, timer int) {
+	time.Sleep(time.Duration(timer) * time.Second)
+	ch <- true
 }
 
-func handler(ch chan bool) {
-	select {
-	case <-ch:
-		fmt.Printf("\nTime Out\nYour Score : %d out of %d \n", score, total)
-		os.Exit(0)
-	}
+func handler(ch chan string, solution string) {
+	var ans string
+	fmt.Scanf("%s", &ans)
+	ch <- ans
 }
 
-func ReadCSV() {
+func QuizGame() {
 	f := flag.String("fp", "./QuizGame/CSV/problems.csv", "Get problem file path")
 	t := flag.Int("timer", 10, "Set timer for quiz")
 	flag.Parse()
 
-	fp, err := os.ReadFile(*f)
+	quiz, err := ReadFile(f)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
-	r := csv.NewReader(strings.NewReader(string(fp)))
-	// fmt.Println(r)
-	var ans string
-
-	ques, err := r.ReadAll()
-	if err != nil {
-		panic(err)
-	}
-	total = len(ques)
+	tc := make(chan bool)
+	ans := make(chan string)
+	score, notAtTimeScore := 0, 0
 	fmt.Println("Quiz is Ready! Are you ready to go:\nPress enter to start")
 	fmt.Scanf("%s")
-	tc := make(chan bool)
-	go Timer(tc, *t)
-	go handler(tc)
-	for _, v := range ques {
-		fmt.Print(v[0], " :")
-		fmt.Scanf("%s\n", &ans)
-		if v[1] == ans {
-			score++
+	for _, ques := range quiz.ques {
+		fmt.Print(ques.Question, ":")
+		go timer(tc, *t)
+		go handler(ans, ques.Solution)
+		select {
+		case <-tc:
+			fmt.Print("\n\nTime's Up\nit wont count, but have a go:")
+			sol := <-ans
+			if sol == ques.Solution {
+				notAtTimeScore++
+			}
+			fmt.Println()
+			break
+		case sol := <-ans:
+			fmt.Print("\nYou're Fast\nGearing up next Question\n\n")
+			<-tc
+			if sol == ques.Solution {
+				score++
+			}
+			break
 		}
 	}
-	fmt.Printf("\nQuiz Complete\nYour Score : %d out of %d \n", score, total)
+
+	fmt.Printf("\nQuiz Complete\nYour Score : %d out of %d \nQuestion Correctly Answered(in time + not in time) :%d\n", score, quiz.Total, score+notAtTimeScore)
 }
--- quiz-game/main.go
+++ quiz-game/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	QuizGame.ReadCSV()
+	QuizGame.QuizGame()
 }
